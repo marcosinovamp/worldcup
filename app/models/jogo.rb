f92520f63998c05e5026860560a1299df6b59767
Marcos Flores <marcoschines@gmail.com>
2022-09-01 13:12:06 -0300

    Selecaos and Jogos atributes

--- app/models/jogo.rb
+++ app/models/jogo.rb
@@ -1,4 +1,16 @@
 class Jogo < ApplicationRecord
     belongs_to :grupo
     has_many :selecaos
+
+    def selecaos
+        [Selecao.find(equipe1).nome, Selecao.find(equipe2).nome]
+    end
+
+    def selecao1
+        Selecao.find(equipe1).nome
+    end
+
+    def selecao2
+        Selecao.find(equipe2).nome
+    end
 end
--- app/models/selecao.rb
+++ app/models/selecao.rb
@@ -2,4 +2,8 @@ class Selecao < ApplicationRecord
   has_many :grupos
   belongs_to :confederation
   has_many :jogos
+  
+  def jogos
+    Jogo.all.select{|j| j.equipe1 == self.id || j.equipe2 == self.id}
+  end
 end
