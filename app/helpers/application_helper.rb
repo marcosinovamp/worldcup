375af47d7a39f01f47f862511bfac71e7158f208
Marcos Flores <marcoschines@gmail.com>
2022-09-02 14:11:48 -0300

    Little alterations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def bradate(data)
+        months = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
+        parts = data.to_s.split('/')
+        return "#{parts[0]} de #{months[parts[1].to_i]} de #{parts[2]} às #{parts[3]}:#{parts[4]}"
+    end
 end
